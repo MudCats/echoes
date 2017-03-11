ff6839e71920147dd984a34747185523134daf25
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-03-11 12:41:05 -0600

    Create server with dependencies and routes.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-//Fill code
+//DEPENDENCIES
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+var app = express();
+
+var appServer = require('./appRoutes.js');
+var authServer = require('./authRoutes.js');
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+app.use('/public', express.static(path.join(__dirname, '/../client')));
+
+// ROUTERS
+app.use('/', authServer);
+app.use('/signin', authServer);
+app.use('/signup', authServer);
+
+// LISTENER
+app.listen(1337, function () {
+  console.log('Server is listening on port 1337.')
+});
