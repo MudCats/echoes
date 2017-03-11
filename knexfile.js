eb25d7ea24bb582b8dc0219ba007480bad284d44
mrabuse <cogglemat@gmail.com>
2017-03-11 16:36:21 -0600

    Refactor knexfile to use env variables

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 // Update with your config settings.
+var env = require('dotenv').config();
+
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      database: 'echoes'
-      username:,
-      password:
+      database: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     debug: true
   },
@@ -14,9 +16,9 @@ module.exports = {
   staging: {
     client: 'pg',
     connection: {
-      database: 'echoes',
-      username:,
-      password:
+      database: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
@@ -30,9 +32,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: 'echoes',
-      username:,
-      password:
+      database: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
