ccb73f3dd3c407b036dd0ecd7b3ce0771fa73cb3
mrabuse <cogglemat@gmail.com>
2017-03-13 16:27:05 -0500

    Refactor knexfile to simplify current environment

--- knexfile.js
+++ knexfile.js
@@ -9,42 +9,40 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: process.env.DB_HOST,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS
+      database: 'echoes'
     },
     debug: true
-  },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      database: process.env.DB_HOST,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      database: process.env.DB_HOST,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+
+  // staging: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: process.env.DB_HOST,
+  //     username: process.env.DB_USER,
+  //     password: process.env.DB_PASS
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+  //
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: process.env.DB_HOST,
+  //     username: process.env.DB_USER,
+  //     password: process.env.DB_PASS
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
