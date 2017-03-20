d87a228a32e13228a5005cf99aec8be1101755e3
mrabuse <cogglemat@gmail.com>
2017-03-20 10:57:57 -0500

    Refactor start files to deploy on heroku

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 var config      = require('../knexfile.js');
-var env         = 'development';
+var env         = 'production';
 var knex        = require('knex')(config[env]);
 
 module.exports = knex;
 
-knex.migrate.latest([config]); 
+knex.migrate.latest([config]);
