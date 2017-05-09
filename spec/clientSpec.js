b56f95ce713473364a81e21532454e44b33ccfc4
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-09 11:34:23 -0500

    Remove spec foler. Files inside are empty

--- spec/clientSpec.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var chai = require('chai');
-var expect = require('chai').expect;
-var should = require('chai').should();
-var server = require('../server/sever.js');
-var request = require('request');
-var chaiHttp = require('chai-http');
-
-chai.use(chaiHttp);
